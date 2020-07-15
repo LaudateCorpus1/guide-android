c578c85dfd468534c8db63215ef41ba055b772a4
Daniel Stone <me@danstone.uk>
2020-07-15 10:14:12 +0100

    Remove ViewModels for basic implementation * Improved error handling in coroutine

--- app/src/main/java/guide/graphql/toc/ui/chapters/ChaptersFragment.kt
+++ app/src/main/java/guide/graphql/toc/ui/chapters/ChaptersFragment.kt
@@ -6,22 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
 import com.google.android.material.transition.MaterialSharedAxis
+import guide.graphql.toc.ChaptersQuery
 import guide.graphql.toc.R
-import guide.graphql.toc.data.Status
+import guide.graphql.toc.data.apolloClient
 import guide.graphql.toc.databinding.ChaptersFragmentBinding
 
 class ChaptersFragment : Fragment() {
 
-    private val viewModel: ChaptersViewModel by viewModels()
-
     private lateinit var binding: ChaptersFragmentBinding
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -44,7 +43,6 @@ class ChaptersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        requireActivity()
         val adapter =
             ChaptersAdapter(
                 requireContext()
@@ -69,21 +67,33 @@ class ChaptersFragment : Fragment() {
         binding.chapters.addItemDecoration(itemDivider)
         binding.chapters.adapter = adapter
 
-        viewModel.chapterList.observe(viewLifecycleOwner, Observer { chapterListResponse ->
-            when (chapterListResponse.status) {
-                Status.SUCCESS -> {
-                    chapterListResponse.data?.let {
-                        adapter.updateChapters(it)
-                    }
+        lifecycleScope.launchWhenStarted {
+            // Loading
+            try {
+                val response = apolloClient.query(
+                    ChaptersQuery()
+                ).toDeferred().await()
+                if (response.hasErrors()) {
+                    throw Exception("Response has errors")
                 }
-                Status.ERROR -> Toast.makeText(
-                    requireContext(),
-                    getString(R.string.graphql_error, chapterListResponse.message),
-                    Toast.LENGTH_SHORT
-                ).show()
-                Status.LOADING -> {
-                }
+                val chapters = response.data?.chapters ?: throw Exception("Data is null")
+                // Success
+                adapter.updateChapters(chapters)
+            } catch (e: ApolloException) {
+                // Error
+                showErrorMessage("GraphQL request failed")
+            } catch (e: Exception) {
+                showErrorMessage(e.message.orEmpty())
             }
-        })
+        }
+    }
+
+
+    private fun showErrorMessage(errorMessage: String) {
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.graphql_error, errorMessage),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
--- app/src/main/java/guide/graphql/toc/ui/sections/SectionsFragment.kt
+++ app/src/main/java/guide/graphql/toc/ui/sections/SectionsFragment.kt
@@ -7,16 +7,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
 import com.google.android.material.transition.MaterialSharedAxis
+import guide.graphql.toc.ChaptersQuery
+import guide.graphql.toc.SectionsQuery
 import guide.graphql.toc.data.Status
+import guide.graphql.toc.data.apolloClient
 import guide.graphql.toc.databinding.SectionsFragmentBinding
 
 class SectionsFragment : Fragment() {
 
-    private val viewModel: SectionsViewModel by viewModels()
 
     private lateinit var binding: SectionsFragmentBinding
     private val args: SectionsFragmentArgs by navArgs()
@@ -56,26 +61,33 @@ class SectionsFragment : Fragment() {
         binding.sections.addItemDecoration(itemDivider)
         binding.sections.adapter = adapter
 
-        viewModel.sectionsList.observe(viewLifecycleOwner, Observer { sectionsResource ->
-            when (sectionsResource.status) {
-                Status.SUCCESS -> {
-                    sectionsResource.data?.let {
-                        adapter.updateSections(it)
-                        binding.spinner.visibility = View.GONE
-                        binding.error.visibility = View.GONE
-                    }
+        lifecycleScope.launchWhenStarted {
+            // Loading
+            binding.spinner.visibility = View.VISIBLE
+            binding.error.visibility = View.GONE
+            try {
+                val response = apolloClient.query(
+                    SectionsQuery(id = args.chapterId)
+                ).toDeferred().await()
+                if (response.hasErrors()) {
+                    throw Exception("Response has errors")
                 }
-                Status.ERROR -> {
-                    showErrorMessage(sectionsResource.message ?: "")
-                }
-                Status.LOADING -> {
-                    binding.spinner.visibility = View.VISIBLE
+                val sections = response.data?.chapter?.sections ?: throw Exception("Data is null")
+                // Success
+                if (sections.size > 1) {
+                    adapter.updateSections(sections)
+                    binding.spinner.visibility = View.GONE
                     binding.error.visibility = View.GONE
+                } else {
+                    throw Exception("No sections")
                 }
+            } catch (e: ApolloException) {
+                // Error
+                showErrorMessage("GraphQL request failed")
+            } catch (e: Exception) {
+                showErrorMessage(e.message.orEmpty())
             }
-        })
-
-        viewModel.chapterId = args.chapterId
+        }
     }
 
     private fun showErrorMessage(error: String) {
