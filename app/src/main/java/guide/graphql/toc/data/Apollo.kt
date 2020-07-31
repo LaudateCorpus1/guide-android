d81ba4f2dce096009428e5f9be80a41ad6270fdc
Loren 🤓 <lorensr@gmail.com>
2020-07-31 16:30:14 -0400

    Add caching

--- app/src/main/java/guide/graphql/toc/data/Apollo.kt
+++ app/src/main/java/guide/graphql/toc/data/Apollo.kt
@@ -1,11 +1,17 @@
 package guide.graphql.toc.data
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy
+import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory
 
 object Apollo {
     val client: ApolloClient by lazy {
+        val cacheFactory =
+            LruNormalizedCacheFactory(EvictionPolicy.builder().maxSizeBytes(10 * 1024 * 1024).build())
+
         ApolloClient.builder()
             .serverUrl("https://api.graphql.guide/graphql")
+            .normalizedCache(cacheFactory)
             .build()
     }
 }
